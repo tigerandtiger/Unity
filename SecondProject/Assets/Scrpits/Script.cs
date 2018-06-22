26f80dfe556ab23f1c7c39d9118bbb90dd123c99
Anatoly Ostrovsky <tigerandtiger77@gmail.com>
2018-06-23 00:03:47 +0300

    2 game

---
+++ SecondProject/Assets/Scrpits/Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+	public Sprite back;
+	public SpriteRenderer thisSprite;
+	public bool bomb;
+	public GameManager gameManager;
+
+
+	void OnMouseDown()
+	{
+		if (!gameManager.endgame) 
+		{
+			thisSprite.sprite = back;
+			if (bomb)
+			{
+				gameManager.endgame = true;
+			}
+		}
+
+	}
+
+
+}
